c8a1f77f2a07694ea1dcf57fee304d88150532e8
nikita <nikitaevtefeev@gmail.com>
2026-04-08 18:54:03 +0300

    11-jinja

---
+++ 11-jinja/1-hello/hello.py
@@ -0,0 +1,13 @@
+from jinja2 import Template
+
+name = 'Bill'
+age = 28
+
+text = f"My name is { name } and I am { age }"
+print(text)
+
+
+tm = Template("My name is {{ name }} and I am {{ age }}")
+msg = tm.render(name=name, age=age)
+
+print(msg)  # My name is Bill and I am 28
