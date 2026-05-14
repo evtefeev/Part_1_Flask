4cc63ac02738da3a862b4a1cc50dc62412b86626
nikita <nikitaevtefeev@gmail.com>
2026-05-14 15:36:14 +0300

    21-security-fix-xss

--- 21-security/2-xss-fixed.py
+++ 21-security/2-xss-fixed.py
@@ -1,23 +1,26 @@
-from flask import Flask, request, make_response, session
+from flask import Flask, request, make_response, g
 import secrets
+import html
+
 app = Flask(__name__)
 
-app.secret_key = "crfg54g54g54g54g545g"
+app.secret_key = "secret"
+
+@app.before_request
+def generate_nonce():
+    g.csp_nonce = secrets.token_urlsafe(16)
 
 @app.after_request
 def apply_csp(response):
-    nonce = secrets.token_urlsafe(16)  # Генеруємо випадковий nonce для дозволених скриптів
-    csp = (
+    response.headers["Content-Security-Policy"] = (
         f"default-src 'self'; "
-        f"script-src 'self' 'nonce-{nonce}'; "
+        f"script-src 'self' 'nonce-{g.csp_nonce}'; "
         f"style-src 'self'; "
         f"frame-ancestors 'none'; "
         f"base-uri 'self'; "
-        f"form-action 'self'"
+        f"form-action 'self'; "
+        f"object-src 'none'"
     )
-    response.headers["Content-Security-Policy"] = csp
-    response.set_cookie('nonce', nonce)
-    session['nonce'] = nonce
     return response
 
 @app.route('/')
@@ -25,30 +28,43 @@ def index():
     user_name = request.args.get('username', '')
     user_code = request.args.get('user_code', '')
 
-    # Логіка сайту, використання вписаних даних.
+    # Экранирование пользовательского ввода
+    user_name = html.escape(user_name)
+    user_code = html.escape(user_code)
 
-    html = f"""
+    html_page = f"""
     <html>
         <body>
             <h1>Ласкаво просимо, користувач!</h1>
+
             <form method="GET">
                 <p>Введіть нікнейм</p>
                 <input type="text" name="username">
+
                 <p>Введіть свій код доступу:</p>
                 <input type="text" name="user_code">
+
                 <input type="submit" value="Надіслати">
             </form>
+
             <div>Дякуємо! {user_name}</div>
 
-            <button onclick="alert('click')"s>press</button>
-            <script nonce="{session.get('nonce')}">
+            <button id="btn">press</button>
+
+            <script nonce="{g.csp_nonce}">
+                document
+                    .getElementById('btn')
+                    .addEventListener('click', () => {{
+                        alert('click')
+                    }})
+
                 alert('Виконання скрипта дозволено')
             </script>
         </body>
     </html>
     """
-    response = make_response(html)
-    return response
+
+    return make_response(html_page)
 
 if __name__ == '__main__':
     app.run(port=5000)
